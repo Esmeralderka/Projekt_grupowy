4050b5194503ace46a205691f0a46ae2f0d434cb
SKrawczyk <UL0237153@edu.uni.lodz.pl>
2021-04-14 15:20:14 +0200

    hej

--- app/src/main/java/com/example/projekt_grupowy/MainActivity.java
+++ app/src/main/java/com/example/projekt_grupowy/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.projekt_grupowy;
-// pozdrawiam wszystkie anime dziewczynki
+// pozdrawiam wszystkie anime dziewczynki!!!   s 1
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
