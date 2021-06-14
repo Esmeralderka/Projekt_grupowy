5ea269b596c8c811c578d0f1f459cfe044bc4278
Weronika Miksa <weronimiksa@wp.pl>
2021-06-14 13:05:06 +0200

    a

--- app/src/main/java/com/example/projekt_grupowy/Export.java
+++ app/src/main/java/com/example/projekt_grupowy/Export.java
@@ -9,6 +9,6 @@ public class Export extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_export);
+        setContentView(R.layout.activity_pick_export);
     }
 }
