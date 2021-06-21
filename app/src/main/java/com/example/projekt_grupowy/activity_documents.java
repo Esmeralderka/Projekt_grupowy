7316de2cf333b2f16374e14b158fd3d41744a8c0
Weronika Miksa <weronimiksa@wp.pl>
2021-06-21 12:50:36 +0200

    inporcik

--- app/src/main/java/com/example/projekt_grupowy/activity_documents.java
+++ app/src/main/java/com/example/projekt_grupowy/activity_documents.java
@@ -1,14 +1,17 @@
 package com.example.projekt_grupowy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import com.example.projekt_grupowy.Adapters.DocumentsAdapter;
@@ -20,15 +23,22 @@ public class activity_documents extends AppCompatActivity {
 
     RecyclerView rv;
     Button addDocumentButton;
+    Button importFileButton;
     ImageView ImageView_LogOut;
+    Intent myFileIntent;
+    TextView tx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_documents);
 
+
+
         rv = (RecyclerView) findViewById(R.id.documentsRV);
+        tx =  findViewById(R.id.textView8);
         addDocumentButton = findViewById(R.id.button_addField);
+
         addDocumentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +61,21 @@ public class activity_documents extends AppCompatActivity {
             }
         });
 
+        importFileButton = findViewById(R.id.button_importFile_2);
+        importFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // MainActivity.logout();
+                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("documents/*");
+                startActivityForResult(Intent.createChooser(myFileIntent, "Select document"),1);
+               // Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                // Uwaga: wyczyszczenie flag zablokuje mozliwosc powrotu do poprzedniego ekranu, ale w zamian korzystajac z przycisku
+                // powrotu wywali uzytkownika do ekranu (zminimalizuje apke)
+               // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+               // startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -69,5 +94,23 @@ public class activity_documents extends AppCompatActivity {
         DocumentsAdapter adapter = new DocumentsAdapter(this);
         rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         rv.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode)
+        {
+            case 10:
+                if(requestCode==RESULT_OK)
+                {
+                    String patch = data.getData().getPath();
+                    tx.setText(patch);
+                }
+        break;
+        }
+
+      //  super.onActivityResult(requestCode, resultCode, data);
     }
 }
