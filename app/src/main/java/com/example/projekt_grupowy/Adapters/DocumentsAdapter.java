4365490dc86fd55dd4ef6b855b4408261dd2ec16
laksispl23 <l.binkowski@o2.pl>
2021-06-22 20:09:15 +0200

    wyszukiwanie

--- app/src/main/java/com/example/projekt_grupowy/Adapters/DocumentsAdapter.java
+++ app/src/main/java/com/example/projekt_grupowy/Adapters/DocumentsAdapter.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,20 +29,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class DocumentsAdapter  extends RecyclerView.Adapter<DocumentsAdapter.ViewHolder>{
+public class DocumentsAdapter  extends RecyclerView.Adapter<DocumentsAdapter.ViewHolder> implements Filterable {
 
     private static final String TAG = "DocumentsAdapter";
     ArrayList<Document> documents;
+    ArrayList<Document> filteredDocuments;
 
     Context context;
     AlertDialog.Builder builder;
 
 
-    public DocumentsAdapter(Context context) {
-        this.documents = MainActivity.appUser.getDocuments();
-        Collections.sort(documents, (Document a1, Document a2) -> a1.getName().compareTo(a2.getName()));
-
+    public DocumentsAdapter(Context context,ArrayList<Document>document){
+        this.documents =document;
         this.context = context;
+        this.filteredDocuments=document;
         builder = new AlertDialog.Builder(context);
 
         Log.d("documents adapter docs:", MainActivity.appUser.toString());
@@ -94,6 +96,7 @@ public class DocumentsAdapter  extends RecyclerView.Adapter<DocumentsAdapter.Vie
     public int getItemCount() {
         return documents.size();
     }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -158,4 +161,37 @@ public class DocumentsAdapter  extends RecyclerView.Adapter<DocumentsAdapter.Vie
         AlertDialog alert = builder.create();
         alert.show();
     }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String Key = constraint.toString();
+                System.out.println("zawartosc Key "+Key);
+                if(Key.isEmpty()){
+                    documents=filteredDocuments;
+                }else{
+                    ArrayList<Document> lstFiltered=new ArrayList<>();
+                    for(Document row:filteredDocuments){
+                        if(row.getName().toLowerCase().contains(Key.toLowerCase())){
+                            lstFiltered.add(row);
+                        }
+                    }
+                    documents=lstFiltered;
+                }
+                FilterResults filterResults=new FilterResults();
+                filterResults.values=documents;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                documents =(ArrayList<Document>)results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+
 }
--- app/src/main/java/com/example/projekt_grupowy/activity_documents.java
+++ app/src/main/java/com/example/projekt_grupowy/activity_documents.java
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 
@@ -15,12 +18,19 @@ import com.example.projekt_grupowy.Adapters.DocumentsAdapter;
 import com.example.projekt_grupowy.Models.Document;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class activity_documents extends AppCompatActivity {
 
     RecyclerView rv;
     Button addDocumentButton;
     ImageView ImageView_LogOut;
+    ArrayList<Document>documents;
+    ArrayList<Document>documentsAll;
+    CharSequence search="";
+    EditText searchBar;
+    String AA;
+    DocumentsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +39,74 @@ public class activity_documents extends AppCompatActivity {
 
         rv = (RecyclerView) findViewById(R.id.documentsRV);
         addDocumentButton = findViewById(R.id.button_addField);
+        documents=MainActivity.appUser.getDocuments();
+
+        Collections.sort(documents, (Document a1, Document a2) -> a1.getName().compareTo(a2.getName()));
+        searchBar=findViewById(R.id.et_search_2);
+        documentsAll=documents;
+        setRv(documentsAll);
+
+
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s);
+                search=s;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+//        searchBar.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//                for(int i=0;i<documentsAll.size();i++){
+//                    System.out.println(documentsAll.get(i).getName());
+//
+//                }
+//           AA=s.toString();
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                System.out.println("koniec");
+//                for(int i=0;i<documentsAll.size();i++){
+//                    if(documentsAll.get(i).getName().contains(AA))
+//                    {
+//                        documentsFiltered.add(documentsAll.get(i));
+//                    }
+//
+//                }
+//
+//                setRv(documentsFiltered);
+//            }
+//
+//        });
+
+
         addDocumentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 navigateToAddDocument();
             }
         });
-        setRv();
 
         ImageView_LogOut = (ImageView) findViewById(R.id.ImageView_LogOut);
 
@@ -53,11 +124,11 @@ public class activity_documents extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setRv();
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        setRv();
+//    }
 
     private void navigateToAddDocument(){
         Intent intent = new Intent(getApplicationContext(), AddDocumentActivity.class);
@@ -65,8 +136,8 @@ public class activity_documents extends AppCompatActivity {
     }
 
 
-    private void setRv(){
-        DocumentsAdapter adapter = new DocumentsAdapter(this);
+    private void setRv(ArrayList<Document>document){
+        adapter= new DocumentsAdapter(this,document);
         rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         rv.setAdapter(adapter);
     }
